ee6cd504b529c942997fa106cf09d88eb10e15fb
Terry Petersen <arigorn_15@yahoo.com>
2023-08-17 13:05:18 -0400

    Finished accordion functionality

--- project_bar.js
+++ project_bar.js
@@ -2,18 +2,21 @@ let projectButtons = document.getElementsByClassName("accordion_trigger");
 
 const displayDescription = (event) => {
     let element = event.currentTarget;
-    if(element.getElementsByClassName("fa-circle-plus")[0].style.display === 'inline-block') {
+    element.classList.toggle("active");
+    let panel = element.nextElementSibling;
+
+    if (!panel.style.maxHeight) {
         for (const projectButton of projectButtons) {
-            projectButton.nextElementSibling.style.display = 'none';
+            projectButton.nextElementSibling.style.maxHeight = null;
             projectButton.getElementsByClassName("fa-circle-plus")[0].style.display = 'inline-block';
             projectButton.getElementsByClassName("fa-circle-minus")[0].style.display = 'none';
         }
-        element.nextElementSibling.style.display = 'flex';
+        panel.style.maxHeight = panel.scrollHeight + "px";
         element.getElementsByClassName("fa-circle-plus")[0].style.display = 'none';
         element.getElementsByClassName("fa-circle-minus")[0].style.display = 'inline-block';
     }
-    else {
-        element.nextElementSibling.style.display = 'none';
+    else  {
+        panel.style.maxHeight = null;
         element.getElementsByClassName("fa-circle-plus")[0].style.display = 'inline-block';
         element.getElementsByClassName("fa-circle-minus")[0].style.display = 'none';
     }
